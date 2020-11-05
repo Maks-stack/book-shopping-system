f0545940590fcf89fb4ffee15219bf0fb08e79e2
jordicalveres <48489259+jordicalveres@users.noreply.github.com>
2020-11-05 18:29:41 +0100

    Add files via upload

---
+++ BookManager.py
@@ -0,0 +1,34 @@
+
+
+class BookManager():
+
+    pass
+
+    def listBooks():
+        print('a')
+
+    def addToCart(bookId):
+        print('a')
+
+    def listCart():
+        print('a')
+
+    def clearCart():
+        print('a')
+
+    def editCart(bookId):
+        print('a')
+
+    def checkout():
+        print('a')
+
+    def searchBook(option, info):
+        print('a')
+
+    def addBook(bookName, bookPrice, bookAuthor):
+        print('a')
+
+    def deleteBook(bookId):
+        print('a')
+
+        
---
+++ main.py
@@ -0,0 +1,12 @@
+from BookManager import BookManager
+
+
+def main():
+    inp = input('What command do you want to execute?')
+    book_manager = BookManager()
+
+    while (inp != "quit"):
+        
+
+
+main()
