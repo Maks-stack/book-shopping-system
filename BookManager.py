d3fc5c992a70730876ec631383a046cbde3df66e
Maximilian Menschel <maxmenschel5@gmail.com>
2020-11-14 19:31:19 +0100

    BookManager

--- BookManager.py
+++ BookManager.py
@@ -1,16 +1,17 @@
 import itertools
+from Book import Book
 
 class BookManager():
 
     id_iter = itertools.count()
 
     def __init__(self):
-        books = {}
-        cart = {}
-
-
-    def listBooks(args = None):
-        print("asd")
+        self.books = {}
+        self.cart = {}   
+    
+    def listBooks(self, args = None):
+        for key, value in self.books.items():
+	        print("ID: " + str(value.id) + "Title:" + value.title + "Price" +  str(value.price) + "Author: " + value.author)
 
     def addToCart(bookId):
         print('a2')
@@ -30,13 +31,17 @@ class BookManager():
     def searchBook(option, info):
         print('a7')
         #next((x for x in test_list if x.value == value), None)
-
-    def addBook(args):
+    
+    def addBook(self, bookname, bookprice, bookauthor):
+        print("adding book")
         self.id = next(self.id_iter)
-        book = Book(bookName, bookPrice, bookAuthor, self.id)
-        self.books.append(book)
+        book = Book(bookname, bookprice, bookauthor, id)
+        self.books.update({id: book})
 
     def deleteBook(bookId):
         print('a9')
+
+    def notAFun(self, args = None):          
+        print('This function is not a correct one') 
 
         
