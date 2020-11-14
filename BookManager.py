fda2c079ac20e5846937b8a2565ea48f2e0e6d41
Maximilian Menschel <maxmenschel5@gmail.com>
2020-11-14 21:18:52 +0100

    BookManager Changes

--- BookManager.py
+++ BookManager.py
@@ -1,18 +1,22 @@
 import itertools
-import uuid
 from Book import Book
 
 class BookManager():
 
-    id_iter = itertools.count()
+    id_iter = itertools.count(1)
 
     def __init__(self):
         self.books = {}
-        self.cart = {}   
+        self.cart = {}
+        
     
     def listBooks(self, args = None):
-        for key, value in self.books.items():
-	        print("ID: " + str(value.id) + " Title:" + value.title + " Price" +  str(value.price) + " Author: " + value.author)
+        if (bool(self.books)): 
+            for value in self.books.values():
+                value.print_book()
+        else:
+            print("No books available")
+	        
 
     def addToCart(bookId):
         print('a2')
@@ -29,17 +33,22 @@ class BookManager():
     def checkout(args = None):
         print('a6')
 
-    def searchBook(option, info):
+    def searchBook(self, args):
         print('a7')
+        print(args)
         #next((x for x in test_list if x.value == value), None)
     
     def addBook(self, bookname, bookprice, bookauthor):
-        id = uuid.uuid4().hex
+        id = next(self.id_iter)
         book = Book(bookname, bookprice, bookauthor, id)
         self.books.update({id : book})
 
-    def deleteBook(bookId):
+    def deleteBook(self, bookId):
         print('a9')
+        bookid = bookId[0]
+        self.books.pop(bookid)
+
+        print(bookid)
 
     def notAFun(self, args = None):          
         print('This function is not a correct one') 
